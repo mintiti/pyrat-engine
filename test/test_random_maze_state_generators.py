dc938e40ae05fbd52ad6f8e37681e1431d8c050f
Kev <kthiebdev@gmail.com>
2022-05-04 21:50:24 +0200

    add test and gitignore change

--- test/test_random_maze_state_generators.py
+++ test/test_random_maze_state_generators.py
@@ -1,10 +1,33 @@
 import pytest
 import random
 from pyrat_engine.init_configs import MazeConfig
-from pyrat_engine.initializer.random_maze_state_generators import RandomMazeStateGenerators
+from pyrat_engine.initializer.random_maze_state_generators import (
+    RandomMazeStateGenerators,
+)
 
 
 def test_generate_random_cheese_list():
     random.seed(1)
-    expectedList = [(10, 7), (4, 2), (17, 13), (8, 3), (13, 12), (15, 14), (6, 1), (15, 12), (6, 3), (15, 0), (6, 15), (12, 13), (9, 2), (19, 0), (2, 15)]
-    assert(RandomMazeStateGenerators.generate_simple_random_cheese_state(maze_config=MazeConfig()).cheeses == expectedList)
+    expected_list = [
+        (10, 7),
+        (4, 2),
+        (17, 13),
+        (8, 3),
+        (13, 12),
+        (15, 14),
+        (6, 1),
+        (15, 12),
+        (6, 3),
+        (15, 0),
+        (6, 15),
+        (12, 13),
+        (9, 2),
+        (19, 0),
+        (2, 15),
+    ]
+    assert (
+        RandomMazeStateGenerators.generate_simple_random_cheese_state(
+            maze_config=MazeConfig()
+        ).cheeses
+        == expected_list
+    )
