42d0b7516f6ef07152880493c9e6804564403a47
Minh Tri <minhtri3007@hotmail.com>
2022-05-19 18:45:19 +0200

    feat: implement move function in the engine itself

--- pyrat_engine/engines/numpy_vectorized/engine.py
+++ pyrat_engine/engines/numpy_vectorized/engine.py
@@ -1,9 +1,14 @@
+import typing
 from typing import List, Tuple
 
 import copy
 
 from pyrat_engine.engines.base import PyratEngine
-from pyrat_engine.engines.numpy_vectorized.helpers import state_from_current_state
+from pyrat_engine.engines.numpy_vectorized.helpers import (
+    current_game_state_from_state,
+    state_from_current_state,
+)
+from pyrat_engine.engines.numpy_vectorized.logic import move
 from pyrat_engine.state.base import CurrentGameState
 from pyrat_engine.types import Coordinates, Move
 
@@ -20,10 +25,15 @@ class NumpyEngine(PyratEngine):
         self._current_state = state_from_current_state(current_game_state)
 
     def get_current_game_state(self) -> CurrentGameState:
-        pass
+        return current_game_state_from_state(self._current_state)
 
     def move(self, p1_move: Move, p2_move: Move) -> Tuple[float, float]:
-        pass
+        previous_scores = self._current_state.game_data.player_scores[:]
+        move(self._current_state, p1_move, p2_move)
+        return typing.cast(
+            Tuple[float, float],
+            tuple(self._current_state.game_data.player_scores - previous_scores),
+        )
 
     def unmove(
         self, p1_move: Move, p2_move: Move, cheeses: List[Coordinates] = None
